3838af6f836d1235d56d0b6ec9208a4b9f93800e
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-12-05 01:38:55 +0100

    updated

--- html-basics/WEEK8/script.js
+++ html-basics/WEEK8/script.js
@@ -2,11 +2,14 @@ function inputEven(number) {
     return number % 2 === 0;
 }
 
+inputEven(6);
+
 function printNumbers(num1, num2) {
     for (let i = num1; i <= num2; i++) {
         console.log(i);
     }
 }
+printNumbers(3, 9);
 
 function sumInBetween(n1, n2) {
     let sum = 0;
@@ -15,3 +18,5 @@ function sumInBetween(n1, n2) {
     }
     return sum;
 }
+
+sumInBetween(6, 12);
